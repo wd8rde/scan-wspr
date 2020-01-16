714353dec681bd35eedcdb8709c091f4d4e784c8
WD8RDE <wd8rde@gmail.com>
2020-01-16 11:33:17 -0500

    Added config file.

--- run_scan_wspr.sh
+++ run_scan_wspr.sh
@@ -7,5 +7,9 @@ sighandler () {
 }
 echo "Starting scan_wspr.sh, logging to /home/pi/scan-wspr/log/scan_wspr.log"
 mkdir -p /home/pi/scan-wspr/log/
-/home/pi/scan-wspr/scan_wspr.sh -c WD8RDE -l EM69sr -i 3 1836600 3592600 7038600 14095600  28124600 >> /home/pi/scan-wspr/log/scan_wspr.log 2>&1 &
+
+#source config variables
+. /home/pi/scan-wspr/scan_wspr.conf
+
+/home/pi/scan-wspr/scan_wspr.sh -c "$CALL" -l "$GRID" -i 3 $FREQLIST >> /home/pi/scan-wspr/log/scan_wspr.log 2>&1 &
 CHILD=$!
