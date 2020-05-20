7b130dd8f9baba7074f9e90b6dd21142aaf49904
WD8RDE <wd8rde@gmail.com>
2020-05-20 07:59:14 -0900

    Check CALL and GRID has been set.

--- install.sh
+++ install.sh
@@ -8,18 +8,27 @@ fi
 TOPDIR="`pwd`"
 
 #install dependancies
+echo -e "\n------------------------------------------"
+echo  "installing dependancies"
+echo -e "------------------------------------------\n"
 sudo apt-get install build-essential cmake libfftw3-dev libusb-1.0-0-dev curl libcurl4-gnutls-dev ntp
 
 #create git directory
 mkdir -p ./git
 
 #clone librtlsdr source
+echo -e "\n------------------------------------------"
+echo "clone librtlsdr source"
+echo -e "------------------------------------------\n"
 cd ./git/
 if [ ! -d "./librtlsdr" ]; then
 	git clone https://github.com/steve-m/librtlsdr.git
 fi
 
 #build librtlsdr
+echo -e "\n------------------------------------------"
+echo "build librtlsdr"
+echo -e "------------------------------------------\n"
 cd ./librtlsdr/
 mkdir -p ./build
 cd ./build/
@@ -30,12 +39,18 @@ sudo ldconfig
 cd "$TOPDIR"
 
 #clone rtlsdr-wsprd sources
+echo -e "\n------------------------------------------"
+echo "clone rtlsdr-wsprd sources"
+echo -e "------------------------------------------\n"
 cd ./git/
 if [ ! -d "./rtlsdr-wsprd" ]; then
 	git clone https://github.com/Guenael/rtlsdr-wsprd
 fi
 
 #build rtlsdr-wsprd
+echo -e "\n------------------------------------------"
+echo "build rtlsdr-wsprd"
+echo -e "------------------------------------------\n"
 cd ./rtlsdr-wsprd/
 make
 sudo mkdir -p /usr/local/bin
@@ -44,15 +59,27 @@ sudo cp ./rtlsdr_wsprd /usr/local/bin/
 cd "$TOPDIR"
 
 #install udev rule for rtl
+echo -e "\n------------------------------------------"
+echo "install udev rule for rtl"
+echo -e "------------------------------------------\n"
 #sudo cp ./etc/udev/rules.d/90-rtl-sdr.rules /etc/udev/rules.d/
 
 #install logrotate conf
+echo -e "\n------------------------------------------"
+echo "install logrotate conf"
+echo -e "------------------------------------------\n"
 sudo cp ./etc/logrotate.d/scan_wspr /etc/logrotate.d/
 
 #create log directory
+echo -e "\n------------------------------------------"
+echo "create log directory"
+echo -e "------------------------------------------\n"
 mkdir -p ./log
 
 #install rc.local file
+echo -e "\n------------------------------------------"
+echo "install rc.local file"
+echo -e "------------------------------------------\n"
 if [ ! -f /etc/rc.local.bak ]; then
 	sudo mv /etc/rc.local /etc/rc.local.bak
 fi
@@ -61,6 +88,7 @@ sudo cp ./etc/rc.local /etc/rc.local
 echo "***************************************************************"
 echo "*"
 echo "* Installation of scan-wspr is successful. "
-echo "* Please reboot "
+echo "* Please Edit /home/pi/scan-wspr/scan_wspr.conf"
+echo "* Then reboot "
 echo "*"
 echo "***************************************************************"
--- run_scan_wspr.sh
+++ run_scan_wspr.sh
@@ -11,5 +11,15 @@ mkdir -p /home/pi/scan-wspr/log/
 #source config variables
 . /home/pi/scan-wspr/scan_wspr.conf
 
+if [ "$GRID"x == "x" ]; then
+	echo "Please edit /home/pi/scan-wspr/scan_wspr.conf and set GRID"
+	exit 1
+fi
+
+if [ "$CALL"x == "x" ]; then
+	echo "Please edit /home/pi/scan-wspr/scan_wspr.conf and set CALL"
+	exit 2
+fi
+
 /home/pi/scan-wspr/scan_wspr.sh -c "$CALL" -l "$GRID" -i "$ITER" $FREQLIST >> /home/pi/scan-wspr/log/scan_wspr.log 2>&1 &
 CHILD=$!
